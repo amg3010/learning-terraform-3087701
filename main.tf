ed19b73d6a18e5fb13f22015d14517e761942398
amg3010 <151525945+amg3010@users.noreply.github.com>
2023-11-21 11:29:55 +0200

    Create security group

--- main.tf
+++ main.tf
@@ -14,11 +14,57 @@ data "aws_ami" "app_ami" {
   owners = ["979382823631"] # Bitnami
 }
 
+<<<<<<< HEAD
+data "aws_vpc" "default" {
+  default = true
+}
+
+==resource "aws_instance" "web" {
+=======
 resource "aws_instance" "blog" {
+>>>>>>> c9186fc4fb580e9bed6fc2fb806d55cca6e64c71
   ami           = data.aws_ami.app_ami.id
   instance_type = var.instance_type
 
+  vpc_security_group_ids = [aws_security_group.blog.id]
   tags = {
     Name = "HelloWorlds"
   }
+}
+
+resource "aws_security_group" {
+    name = "blog"
+    description = "Allow http and https in. Allow everything out"
+
+    vpc_id=data.aws_vpc.default.id
+}
+
+resource "aws_security_group_rule" "blog_hhtp_in"{
+  type        = "ingress"
+  from_port   = 80
+  to_port     = 80
+  protocol    = "tcp"
+  cidr_blocks = ["0.0.0.0/0"]
+
+  security_group_id = aws_security_group.blog.id
+}
+
+resource "aws_security_group_rule" "blog_hhtps_in"{
+  type        = "ingress"
+  from_port   = 443
+  to_port     = 443
+  protocol    = "tcp"
+  cidr_blocks = ["0.0.0.0/0"]
+
+  security_group_id = aws_security_group.blog.id
+}
+
+resource "aws_security_group_rule" "blog_eveything_out"{
+  type        = "engress"
+  from_port   = 0
+  to_port     = 0
+  protocol    = "-1"
+  cidr_blocks = ["0.0.0.0/0"]
+
+  security_group_id = aws_security_group.blog.id
 }
